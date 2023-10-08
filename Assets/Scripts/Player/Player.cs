6e66d6b4fecf74512ee5da77bd418c1d29985a7c
Khang Duong <khangvngm@gmail.com>
2023-10-09 00:48:50 +0700

    _Khang_01@Movement&Attack

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,15 +1,178 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngineInternal;
 
 public class Player : SingletonMonobehavious<Player>
 {
+    [SerializeField]private Transform groundCheck;
+
     private Rigidbody2D rigidbody2D;
+
+    private float Horizontal;
 
     private void Awake()
     {
         base.Awake();
 
-        rigidbody2D = this.GetComponent<Rigidbody2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        Settings.extraJump = Settings.extraJumpValue;
+    }
+
+    private void FixedUpdate()
+    {
+        // Cấm hành động khi dash
+        if (Settings.isDasing)
+        {
+            return;
+        }
+
+        // Cấm hành động khi tấn công  
+        if (Settings.isAttack)
+        {
+            return;
+        }
+        PlayerMovement();
+    }
+    private void Update()
+    {
+        // Cấm hành động khi dash 
+        if (Settings.isDasing)
+        {
+            return;
+        }
+
+        // Cấm hành động khi tấn công  
+        if (Settings.isAttack)
+        {
+            return;
+        }
+        Flip();
+        PlayerJump();
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Settings.canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
+        // không cho tấn công khi chưa hồi chiêu
+        if (!Settings.canAttack)
+        {
+            return;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(Attack(0));
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            StartCoroutine(Attack(1));
+        }
+    }
+
+    // Lật mặt
+    private void Flip()
+    {
+        if(Settings.isFacingRight && Horizontal<0f || !Settings.isFacingRight && Horizontal > 0f)
+        {
+            Vector2 localScale = transform.localScale;
+            Settings.isFacingRight = !Settings.isFacingRight;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
+    }
+
+    /// <summary>
+    /// Nhận vào giá trị là chuột trái hay phải,trả về animation tương ứng, trả về cờ sát thương mạnh hay yếu
+    /// </summary>
+    private IEnumerator Attack(int typeAttack)
+    {
+            GameObject sword = transform.Find("Attack").gameObject;
+
+            Settings.canAttack = false;
+            Settings.isAttack = true;
+
+            // test cho animation 
+            sword.SetActive(true);
+
+            if (typeAttack == 0)
+            {
+                Settings.normalAttack = true;
+                /// Sát thương gây ra 
+                /// 
+                Damage(0);
+                yield return new WaitForSeconds(Settings.normalAttackTime);
+                Settings.normalAttack = false;
+                Settings.isAttack = false;
+                sword.SetActive(false);
+                yield return new WaitForSeconds(Settings.normalAttackCooldown);
+            }
+            else if (typeAttack == 1)
+            {
+                Settings.strongAttack = true;
+                /// Sát thương gây ra 
+                /// 
+                Damage(0);
+                yield return new WaitForSeconds(Settings.strongAttackTime);
+                Settings.strongAttack = false;
+                Settings.isAttack = false;
+                sword.SetActive(false);
+                yield return new WaitForSeconds(Settings.strongAttackCooldown);
+            }
+            Settings.canAttack = true;
+    }
+
+    /// <summary>
+    ///  Damage
+    /// </summary>
+    private void Damage(int dmg)
+    {
+
+    }
+
+    private void PlayerMovement()
+    {
+        Settings.isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.5f, LayerMask.GetMask(Settings.groundLayerMask));
+
+        float move = Input.GetAxis("Horizontal");
+
+        rigidbody2D.velocity = new Vector2(move * Settings.speedMove, rigidbody2D.velocity.y);
+
+        Horizontal = move;
+    }
+
+    // Nhảy 
+    private void PlayerJump()
+    {
+        if(Settings.isGrounded == true)
+        {
+            Settings.extraJump = Settings.extraJumpValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && Settings.extraJump > 0)
+        {
+            rigidbody2D.velocity = Vector2.up * Settings.jumpForce;
+            Settings.extraJump--;
+        } else if (Input.GetKeyDown(KeyCode.W) && Settings.extraJump == 0 && Settings.isGrounded == true)
+        {
+            rigidbody2D.velocity = Vector2.up * Settings.jumpForce;
+        }
+    }
+
+    // dash 
+    private IEnumerator Dash()
+    {
+        Settings.canDash = false;
+        Settings.isDasing = true;
+
+        float originalGravity = rigidbody2D.gravityScale;
+        rigidbody2D.gravityScale = 0f;
+        rigidbody2D.velocity = new Vector2(transform.localScale.x*Settings.dashForce, 0f);
+        yield return new WaitForSeconds(Settings.dashingTime);
+        Settings.isDasing = false;
+        rigidbody2D.gravityScale = originalGravity;
+        yield return new WaitForSeconds(Settings.dashCooldown);
+        Settings.canDash = true;
     }
 }
