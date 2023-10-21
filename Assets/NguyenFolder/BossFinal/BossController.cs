f912b960d476f7ca3160ae895bd6fc1a2319b86d
LeThienTrungNguyen <112458785+LeThienTrungNguyen@users.noreply.github.com>
2023-10-22 00:54:53 +0700

    Add boss final

---
+++ Assets/NguyenFolder/BossFinal/BossController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    [SerializeField] float jumpHeight;
+    Rigidbody2D rb2d;
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    [ContextMenu("JumpToTarget")]
+    public void JumpToTarget()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player) {
+            float distanceX = player.transform.position.x - transform.position.x;
+            float middlePointX = distanceX / 2;
+            float jumpHeightY = transform.position.y + jumpHeight;
+
+            Vector2 jumpVector = new Vector2(middlePointX, jumpHeightY);
+            rb2d.velocity = jumpVector;
+            Debug.Log($"found {player.name}");
+            
+
+            Debug.Log($"Found {player.name}");
+        }
+        
+    }
+
+    public float jumpForce = 5f;
+    public Transform player;
+    //private Rigidbody2D rb2d;
+
+
+    private void Update()
+    {
+        
+    }
+}
