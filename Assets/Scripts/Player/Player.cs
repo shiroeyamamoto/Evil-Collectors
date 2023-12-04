f926e17a9f7a572b5f0035b731a3a54333bb73a5
dat <nguyenpdat98@gmail.com>
2023-12-04 21:29:24 +0700

    Revert "Revert "Revert "Squashed commit of the following:"""
    This reverts commit 65062df052c2cfcca5aa9c8495876ddf0222bb7b.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -25,10 +25,6 @@ public class Player : SingletonMonobehavious<Player>, IInteractObject
     
     public List<SkillBase> SkillList { get; private set; }
 
-    private void Start()
-    {
-        rb2d = GetComponent<Rigidbody2D>();
-    }
     public void Init(SO_PlayerData playerData)
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -278,7 +274,7 @@ public class Player : SingletonMonobehavious<Player>, IInteractObject
             undeadCounter = 0;
             Settings.zombieMode = false;
             spriteRendererPlayer.color = Settings.playerColor;
-            //hatSpriteRenderer.color = Settings.playerHatColor;
+            hatSpriteRenderer.color = Settings.playerHatColor;
             OnDamagedTwinkling?.Invoke(1);
             OnDamagedTwinkling?.Invoke(2);
             //StopCoroutine(Twinkling());
@@ -309,6 +305,6 @@ public class Player : SingletonMonobehavious<Player>, IInteractObject
 
     public void OnDamaged(float damage, bool value)
     {
-        throw new NotImplementedException();
+        return;
     }
 }
